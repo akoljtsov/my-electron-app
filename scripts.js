012a35da08ce714ded07b9108faffe0b3e6763cc
JS-007 <akoljtsov@example.com>
2024-09-29 12:16:27 +0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+document.getElementById('registrationForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+  
+    if (password !== confirmPassword) {
+      alert('Passwords do not match');
+    } else {
+      alert(`Registration successful for ${email}`);
+    }
+  });
+  
